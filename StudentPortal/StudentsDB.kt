98ad4914d66cc813a04009d194d188481aca3e0e
Prashanth003007 <129504948+Prashanth003007@users.noreply.github.com>
2024-08-13 14:11:34 +0530

    Add files via upload

--- StudentPortal/StudentsDB.kt
+++ StudentPortal/StudentsDB.kt
@@ -1,5 +1,7 @@
 package StudentPortal
 
+import com.sun.jdi.connect.Transport
+
 data class Students(
     val studentId : String,
     val studentName : String,
@@ -10,8 +12,35 @@ data class Students(
     var studentType : Type
 )
 
-open class Type{}
+interface Type{
+    fun studentFees() : Float
+}
 
-class Dayscholar(val transport:String,val distance:Float,val busType:String) : Type(){}
+class Dayscholar(val transport:String,val distance:Float,val busType:String) : Type{
+    override fun studentFees(): Float {
+        when(transport)
+        {
+            "collegeBus" -> {
+                when(busType){
+                    "A/C" -> return distance*10+5000
+                    "non-A/C" -> return distance*10
+                }}
 
-class Hosteller(val roomType:String,val bedType:Int) : Type(){}
+            "ownTransport" -> return 0
+        }
+    }
+}
+
+class Hosteller : Type(){}
+
+class Academics(val studentRank: Int):Type{
+    override fun studentFees():Float {
+        when(studentRank){
+            in 1..40000 -> return 96000
+            in 40001..80000 -> return 150000
+            in 80001..100000 -> return 200000
+            else -> return 250000
+        }
+    }
+}
+
