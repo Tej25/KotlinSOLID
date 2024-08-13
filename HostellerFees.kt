86bcf32ce910d8f2bf83d08c96651c4669dbb45c
V Divakar <122922211+Divakar23@users.noreply.github.com>
2024-08-13 14:30:38 +0530

    Add files via upload

---
+++ HostellerFees.kt
@@ -0,0 +1,37 @@
+package `KotlinSOLID-main`.`KotlinSOLID-main`.StudentPortal
+
+fun StudentPortal.Hosteller.roomFee():Float{
+    if(roomType == "AC"){
+        return when(bedType){
+            1 -> 90000.00f
+            2 -> 75000.00f
+            3 -> 60000.00f
+            4 -> 50000.00f
+            6 -> 40000.00f
+            else -> 0.00f
+        }
+    }
+    else if(roomType == "NONAC"){
+        return when(bedType){
+            1 -> 60000.00f
+            2 -> 50000.00f
+            3 -> 40000.00f
+            4 -> 30000.00f
+            6 -> 20000.00f
+            else -> 0.00f
+        }
+    }
+    else{
+        println("Specified Type is not available")
+        return 0.0f
+    }
+}
+
+fun StudentPortal.Hosteller.messFee():Float{
+    return when(messType){
+        "VEG" -> 35000.00f
+        "NONVEG" -> 55000.00f
+        "SPECIAL" -> 70000.00f
+        else -> 0.00f
+    }
+}
