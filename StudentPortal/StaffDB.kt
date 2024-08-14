8b335099ac29b052905fd81f3e73a2e674515651
Prashanth003007 <129504948+Prashanth003007@users.noreply.github.com>
2024-08-14 12:09:02 +0530

    Add files via upload

---
+++ StudentPortal/StaffDB.kt
@@ -0,0 +1,10 @@
+package StudentPortal
+
+data class Staff(
+    val staffId : String,
+    val staffName : String,
+    val staffAge : Int,
+    val staffDepartment : String,
+    val yearOfExperience : Int,
+)
+
--- StudentPortal/Staffs.kt
+++ StudentPortal/Staffs.kt
@@ -2,47 +2,63 @@ package StudentPortal
 
 interface Staffs{
     fun salaryCalculation():Float
-    //fun gradeAllocation():String
-    //THIS WILL THROUGH ERROR IN NonTeachingStaffs if UNCOMMENTED SINCE IT IS NOT USED
+    //    fun gradeAllocation():String
+    // THIS WILL THROUGH ERROR IN NonTeachingStaffs if UNCOMMENTED SINCE IT IS NOT USED
 }
 
 interface ProfessorGrade{
+
     fun gradeAllocation():String
+
 }
 
-class SalaryCalculatorForProfessor(_grade: String): Staffs{
-    var grade = _grade
-    override fun salaryCalculation(): Float {
-        return when(grade) {
-            "Trainee" -> 300000f
-            "Assistant Professor Grade 2" -> 450000f
-            "Assistant Professor Grade 1" -> 600000f
-            "Professor Grade 2" -> 800000f
-            "Professor Grade 1" -> 1000000f
-            else -> {
-                println("The Grade doesn't exist")
-                0f
+class Professors(val yearsOfExperience:Int) : Staffs,ProfessorGrade {
+
+    var grade = Professors(yearsOfExperience).gradeAllocation()
+
+    override fun salaryCalculation():Float{
+
+        return when(grade){
+            "Professor Grade 1"  -> 500000f
+            "Professor Grade 2" -> 300000f
+            "Assistant Professor Grade 1" -> 200000f
+            "Assistant Professor Grade 2" -> 100000f
+            "Trainee" -> 35000f
+            else -> 0f
+        }
+
+    }
+
+    override fun gradeAllocation() : String {
+
+        return when(yearsOfExperience)
+        {
+           in 15..20 -> "Professor Grade 1"
+           in 10..14 -> "Professor Grade 2"
+           in 6..9 -> "Assistant Professor Grade 1"
+           in 3..5 ->  "Assistant Professor Grade 2"
+            in 1..2 -> "Trainee"
+            else -> "No Record Found"
+
+        }
+
+    }
+
+}
+
+class NonTeachingStaffs(val staffDepartment : String): Staffs{
+
+    var designation = staffDepartment
+        override fun salaryCalculation(): Float {
+            return when(designation){
+                "Gardening" -> 200000f
+                "Cleaning" -> 150000f
+                "Management" -> 350000f
+                "Technical" -> 600000f
+                else ->{
+                    println("The Designation doesn't exist")
+                    0f
+                }
             }
         }
-    }
-}
-
-class SalaryCalculatorForStaff(_designation : String):Staffs{
-    var designation = _designation
-    override fun salaryCalculation(): Float {
-        return when(designation){
-            "Gardening" -> 200000f
-            "Cleaning" -> 150000f
-            "Management" -> 350000f
-            "Technical" -> 600000f
-            else ->{
-                println("The Designation doesn't exist")
-                0f
-            }
-        }
-    }
-}
-
-fun main(args: Array<String>){
-
 }
