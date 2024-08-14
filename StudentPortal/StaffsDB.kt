d84c3e4fd6a64e821f0b4390ce0cacec8c59b143
Tejas <62961560+Tej25@users.noreply.github.com>
2024-08-14 14:08:39 +0530

    Add files via upload

---
+++ StudentPortal/StaffsDB.kt
@@ -0,0 +1,9 @@
+package StudentPortal
+
+data class Staffs(
+    val staffId : String,
+    val staffName : String,
+    val staffAge : Int,
+    val staffDepartment : String,
+    val yearOfExperience : Int,
+)
